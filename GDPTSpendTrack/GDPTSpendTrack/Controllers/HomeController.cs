21487023612c895778ad9a37c1e19bd49a6b9f2e
Ngoc-Binh Tran <binht@fraxion.biz>
2022-02-16 15:50:15 -0800

    First version of GDPT SpendTrack

--- GDPTSpendTrack/GDPTSpendTrack/Controllers/HomeController.cs
+++ GDPTSpendTrack/GDPTSpendTrack/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GDPTSpendTrack.Models;
 
 namespace GDPTSpendTrack.Controllers
 {
@@ -15,16 +16,29 @@ namespace GDPTSpendTrack.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "GDPT Spend Tracker.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "GDPT Spend Tracker.";
 
             return View();
+        }
+
+        public ActionResult GetTransactions()
+        {
+            var transactions = new List<Transaction>
+            {
+                new Transaction {Date = new DateTime(2022, 2, 6), Description = string.Empty, Amount = 25, Notes = "From Vy's mom"}
+                , new Transaction {Date = new DateTime(2022, 2, 7), Description = string.Empty, Amount = 360, Notes = "From Sophia's mom"}
+                , new Transaction {Date = new DateTime(2022, 2, 8), Description = string.Empty, Amount = 100, Notes = "From chi Binh"}
+                , new Transaction {Date = new DateTime(2022, 2, 9), Description = string.Empty, Amount = 100, Notes = "From chi Ngoc"}
+            };
+
+            return Json(transactions, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ GDPTSpendTrack/GDPTSpendTrack/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GDPTSpendTrack.Models
+{
+    public enum TransactionType
+    {
+        Unknown = 0
+        , Donation = 1
+        , Expense = 2
+    }
+
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Notes { get; set; }
+    }
+}
